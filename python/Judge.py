e3f68aa6ab79335ccffe0fba26d42a7298a6553b
Hosung <an3735297@naver.com>
2023-11-10 12:05:26 +0900

    파이썬 실행기의 표준 출력을 표준화하였습니다.

--- python/Judge.py
+++ python/Judge.py
@@ -1,45 +1,56 @@
 import time
 import tracemalloc
 import sys
+import io
 
 
+# 양식
+# 0,1 : 1은 정상, 0은 오류
+# error : 오류 종류 ( 있다면 이 줄에서 끝 / 없으면 이 줄 생략하고 다음 줄 )
+# time : 실행 시간 ( ms )
+# memory : 메모리 사용량 ( byte )
+# output : 출력
 def run_script(script_path, input_file_path, time_limit=1, memory_limit=10000000):
-    # Memory usage tracking start
-    tracemalloc.start()
-
-    # Redirect stdin to read from the input file
     original_stdin = sys.stdin
     sys.stdin = open(input_file_path, 'r')
 
-    start_time = time.time()  # Start time
+    output_capture = io.StringIO()
+    sys.stdout = output_capture
+
+    start_time = time.time()
+    tracemalloc.start()
     try:
-        # Execute the script
         with open(script_path, 'r') as file:
             exec(file.read(), {'__name__': '__main__'})
     except Exception as e:
-        print(f"An error occurred: {e.__class__.__name__}")
+        error_occurred = e.__class__.__name__
+        result = f"0\n{error_occurred}"
+        print(result)
+        return
     finally:
         sys.stdin.close()
-        sys.stdin = original_stdin  # Reset stdin to its original value
+        sys.stdin = original_stdin
+        sys.stdout = sys.__stdout__
 
-    end_time = time.time()  # End time
-    execution_time = end_time - start_time  # Calculate execution time
-
-    # Check for time limit exceedance
-    if execution_time > time_limit:
-        print(f"Execution time exceeded the limit of {time_limit} seconds")
-
-    # Memory usage
+    end_time = time.time()
+    execution_time = end_time - start_time
     current, peak = tracemalloc.get_traced_memory()
-    tracemalloc.stop()  # Stop memory tracking
+    tracemalloc.stop()
 
-    # Check for memory limit exceedance
+    if execution_time > time_limit:
+        result = f"0\ntime limit exceeded"
+        print(result)
+        return
+
     if peak > memory_limit:
-        print(f"Memory usage exceeded the limit of {memory_limit} bytes")
+        result = f"0\nmemory limit exceeded"
+        print(result)
+        return
 
-    # Print execution time and memory usage
-    print(f"Execution time: {execution_time} seconds")
-    print(f"Peak memory usage: {peak} bytes")
+    result = f"1\n{int(execution_time * 1000)}\n{peak}\n{output_capture.getvalue()}"
+    print(result)
+    return
+
 
 # Usage
 run_script('Main.py', 'input.txt')
