4ff0fa7371aeb2d3bcc853d9c226bbd7be334017
Hosung <an3735297@pusan.ac.kr>
2023-11-11 15:08:26 +0900

    파이썬 실행기 : json 을 만드는 방식에서 출력하는 방식으로 변경

--- python/PythonRunner.py
+++ python/PythonRunner.py
@@ -1,26 +1,18 @@
 import time
 import sys
 import io
-import json
-import uuid
 
 MAIN_PATH = "Main.py"
 INPUT_PATH = "input.txt"
 
 
-def create_result(success, error_name, execution_time_in_ms, output):
-    data = {
-        "success": success,
-        "error_name": error_name,
-        "execution_time_in_ms": execution_time_in_ms,
-        "output": output
-    }
+def create_result(success, error, execution_time_in_ms, output):
+    print(success)
+    print(error)
+    print(execution_time_in_ms)
+    print(output)
+    return
 
-    filename = uuid.uuid4();
-    with open(f"result/{filename}.json", "w") as file:
-        json.dump(data, file)
-
-    return filename
 
 def run_script(time_limit_in_ms):
     original_stdin = sys.stdin
@@ -30,29 +22,42 @@ def run_script(time_limit_in_ms):
     sys.stdout = output_capture
 
     start_time = time.time()
+    error = None
 
     try:
         with open(MAIN_PATH, 'r') as file:
-            exec(file.read(), {'__name__': '__main__'})
-    except Exception as e:
-        error_occurred = e.__class__.__name__
+            script_content = file.read()
+        compile(script_content, MAIN_PATH, 'exec')
+    except SyntaxError:
+        error = "compile error"
+
+    if error:
         sys.stdout = original_stdout
         sys.stdin = original_stdin
-        print(create_result(False, error_occurred, 0, ""))
+        print(1)
+        create_result(False, error, 0, "")
         return
-    finally:
-        sys.stdout = original_stdout
-        sys.stdin = original_stdin
+
+    try:
+        # Executing the script
+        exec(script_content, {'__name__': '__main__'})
+    except Exception:
+        error = "runtime error"
+
+    sys.stdout = original_stdout
+    sys.stdin = original_stdin
 
     end_time = time.time()
     execution_time_in_ms = int((end_time - start_time) * 1000)
 
     if execution_time_in_ms > time_limit_in_ms:
-        print(create_result(False, "TimeLimitExceededError", execution_time_in_ms, ""))
-        return
+        error = "time limit exceeded"
 
-    output = output_capture.getvalue()
-    print(create_result(True, "", execution_time_in_ms, output))
+    if error:
+        create_result(False, error, execution_time_in_ms, "")
+    else:
+        output = output_capture.getvalue()
+        create_result(True, "", execution_time_in_ms, output)
 
 
 # Usage
