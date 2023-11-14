0daa8a5c5609edc913cfba32aa5409090ce9c5f9
Hosung <an3735297@naver.com>
2023-11-14 11:58:16 +0900

    python 에서 다른 언어도 실행할 수 있도록 기존과 다른 버전 생성

---
+++ pythonv2/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> arr;
+    for (int i=0;i<100000000;i++) {
+        arr.push_back(i);
+    }
+    int n = 0;
+    int a; cin >> a;
+    int b; cin >> b;
+    cout << a + b << endl;
+}
